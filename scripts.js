ceb56b6355bb339a0a78896a5d3b05fb0bb75fb1
Sina Wolper <sina@thewolpers.com>
2022-10-05 17:38:44 -0700

    Create playRound function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
+const hands = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-	const hands = ["Rock", "Paper", "Scissors"];
 	const choice = Math.floor(Math.random() * hands.length);
-	console.log(hands[choice]);
+	return(hands[choice]);
+}
+
+function playRound(playerSelection,computerSelection) {
+	computerSelection = getComputerChoice();
+
+	if (
+		(playerSelection === "Rock" && computerSelection === "Scissors") ||
+		(playerSelection === "Paper" && computerSelection === "Rock") ||
+		(playerSelection === "Scissors" && computerSelection === "Paper")
+	) {
+		console.log("You win! " + playerSelection + " beats " + computerSelection + "!");
+	} else if (
+		(playerSelection === "Rock" && computerSelection === "Rock") ||
+		(playerSelection === "Paper" && computerSelection === "Paper") ||
+		(playerSelection === "Scissors" && computerSelection === "Scissors")
+	) {
+		console.log("Tie game! You both played " + playerSelection);
+	} else {
+		console.log("You lose! " + playerSelection + " loses to " + computerSelection + "!");
+	}
 }
